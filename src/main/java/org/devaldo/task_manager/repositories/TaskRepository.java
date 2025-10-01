251e14c2d33d627c8e6d797733e9c593a4b3f4c4
Aldo Jure <dev.aldojure@gmail.com>
2025-10-01 13:20:27 -0300

    fix: write entire task repository again

--- src/main/java/org/devaldo/task_manager/repositories/TaskRepository.java
+++ src/main/java/org/devaldo/task_manager/repositories/TaskRepository.java
@@ -1,22 +1,14 @@
 package org.devaldo.task_manager.repositories;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
-import org.devaldo.task_manager.Enums.TaskStatus;
 import org.devaldo.task_manager.entities.Task;
 
 public interface TaskRepository {
 
-    Task createTask(Long id, String title, String description, LocalDate dueDate, TaskStatus status);
-
-    Task getTaskById(Long id);
-
-    List<Task> getAllTasks();
-
-    Task removeTask(Long id);
-
-    Task completeTask(Long id);
-
-    List<Task> filterTasksByStatus(TaskStatus status);
+    Task save(Task task);                 
+    Optional<Task> findById(Long id);
+    List<Task> findAll();
+    void remove(Long id);
 }
